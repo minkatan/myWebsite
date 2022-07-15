47d38c42cd0b112780ceb91eeadd6c0b715e8322
minkatan <minka.tan@gmail.com>
2022-07-15 10:42:07 -0400

    add animation;

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 // add mobile navbar button
-
 const menuBtn = document.getElementById('menu')
 const mobile = document.getElementById('mobile-navbar')
 
@@ -10,3 +9,39 @@ menuBtn.addEventListener('click',function(){
         mobile.classList.remove('hidden')
     }
 })
+
+// scroll reveal
+function revealAboutMe() {
+    const reveals = document.getElementById('about-me')
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals.getBoundingClientRect().top;
+    let elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals.classList.add("animate__animated", "animate__backInDown");
+    } else {
+      reveals.classList.remove("animate__animated", "animate__backInDown");
+    }
+  }
+  window.addEventListener("scroll", revealAboutMe);
+
+  // To check the scroll position on page load
+  revealAboutMe();
+
+// scroll reveal
+function revealContact() {
+    const reveals = document.getElementById('contact')
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals.getBoundingClientRect().top;
+    let elementVisible = 50;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals.classList.add("animate__animated", "animate__zoomInDown");
+    } else {
+      reveals.classList.remove("animate__animated", "animate__zoomInDown");
+    }
+  }
+  window.addEventListener("scroll", revealContact);
+
+  // To check the scroll position on page load
+  revealContact();
